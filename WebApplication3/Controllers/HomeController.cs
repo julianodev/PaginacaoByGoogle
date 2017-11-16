b0f965d7c4673921b25b02645cff3f38a27c2aa0
Lucas juliano <lucas.juliano@live.com>
2017-11-16 11:44:19 -0200

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(int? pagina)
+        {
+            //Gera uma quantidade X de items
+            var items = Enumerable.Range(1, 150).Select(x => "Item " + x);
+
+            //Efetua a paginação dos itens
+            var paginador = new Paginador(items.Count(), pagina);
+
+
+            var viewModel = new IndexViewModel
+            {
+                Items = items.Skip((paginador.PaginaAtual - 1) * (paginador.ItemsPorPagina))
+                .Take(paginador.ItemsPorPagina),
+                Paginador = paginador
+            };
+
+            return View(viewModel);
+        }
+
+       
+    }
+}
---
+++ WebApplication3/Models/IndexViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication3.Models
+{
+
+    public sealed class IndexViewModel
+    {
+        public IEnumerable<string> Items { get; set; }
+        public Paginador Paginador { get; set; }
+    }
+
+    /// <summary>
+    /// Efetua a Paginação dos items
+    /// </summary>
+    public sealed class Paginador
+    {
+        public Paginador(int qtdRegistros, int? pagina, int qtdPagina = 10)
+        {
+            EfetuaPaginacao(qtdRegistros, pagina, qtdPagina);
+        }
+
+        public int TotalRegistros { get; private set; }
+        public int PaginaAtual { get; private set; }
+        public int ItemsPorPagina { get; private set; }
+        public int TotalPaginas { get; private set; }
+        public int PrimeiraPagina { get; private set; }
+        public int UltimaPagina { get; private set; }
+
+        private void EfetuaPaginacao(int qtdRegistros, int? pagina, int qtdPagina = 10)
+        {
+            var totalPaginas = (int)Math.Ceiling((decimal) qtdRegistros / qtdPagina);
+            var paginaAtual = pagina != null ? (int)pagina : 1;
+            var primeiraPagina = paginaAtual - 5;
+            var ultimaPagina = paginaAtual + 4;
+
+            if (primeiraPagina <= 0)
+            {
+                ultimaPagina -= (primeiraPagina--);
+                primeiraPagina = 1;
+            }
+            if (ultimaPagina > totalPaginas)
+            {
+                ultimaPagina = totalPaginas;
+
+                if (ultimaPagina > 10)
+                {
+                    primeiraPagina = ultimaPagina - 9;
+                }
+            }
+
+            TotalRegistros = qtdRegistros;
+            PaginaAtual = paginaAtual;
+            ItemsPorPagina = qtdPagina;
+            TotalPaginas = totalPaginas;
+            PrimeiraPagina = primeiraPagina;
+            UltimaPagina = ultimaPagina;
+        }
+    }
+}
